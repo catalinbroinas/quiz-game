45a60218fef552b24d96353ca98b48471c5eca96
catalinbroinas <catalinbroinas@yahoo.com>
2026-03-04 16:12:23 +0200

    feat(GameSettings): add static UI

--- src/react/components/features/Game/Game.jsx
+++ src/react/components/features/Game/Game.jsx
@@ -1,8 +1,11 @@
+import GameSettings from "./GameSettings/GameSettings";
 
 function Game() {
   return (
     <div className="game">
       <h1 className="game__title">Quiz Game</h1>
+
+      <GameSettings />
     </div>
   );
 }
---
+++ src/react/components/features/Game/GameSettings/GameSettings.jsx
@@ -0,0 +1,12 @@
+
+function GameSettings() {
+  return (
+    <div className="game__settings">
+      <form className="form-container">
+        <button className="btn-primary">Start Quiz</button>
+      </form>
+    </div>
+  );
+}
+
+export default GameSettings;
