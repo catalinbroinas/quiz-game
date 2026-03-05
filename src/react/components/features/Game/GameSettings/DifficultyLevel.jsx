ca7f6ee3de74118456e0fe39959a4d51ef530a03
catalinbroinas <catalinbroinas@yahoo.com>
2026-03-05 12:51:38 +0200

    feat(DifficultyLevel): add controlled component

--- src/react/components/features/Game/GameSettings/DifficultyLevel.jsx
+++ src/react/components/features/Game/GameSettings/DifficultyLevel.jsx
@@ -1,10 +1,18 @@
 
-function DifficultyLevel() {
+function DifficultyLevel({ difficulty, onDifficultyChange }) {
   return (
     <div className="form-select-group">
-      <label htmlFor="difficulty-level" className="form-label-text">Difficulty level:</label>
+      <label
+        htmlFor="difficulty-level"
+        className="form-label-text"
+      >Difficulty level:</label>
 
-      <select id="difficulty-level" className="form-select">
+      <select
+        id="difficulty-level"
+        className="form-select"
+        value={difficulty}
+        onChange={(e) => onDifficultyChange(e.target.value)}
+      >
         <option value="easy">Easy</option>
         <option value="medium">Medium</option>
         <option value="hard">Hard</option>
