3a434a80289a75b8ef4e0fcfb16b1d4e7818d81b
catalinbroinas <catalinbroinas@yahoo.com>
2026-03-05 14:19:30 +0200

    feat(data/questions): add initial quiz questions

---
+++ src/react/data/questions.js
@@ -0,0 +1,22 @@
+import {
+  CATEGORY_OPTIONS,
+  DIFFICULTY_OPTIONS
+} from "../constants/quizOptions";
+
+const questions = [
+  {
+    id: 1,
+    question: 'What does HTML stand for?',
+    category: CATEGORY_OPTIONS[0].value,
+    difficulty: DIFFICULTY_OPTIONS[0].value,
+    answers: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks and Text Markup Language",
+      "Hyper Tool Multi Language"
+    ],
+    correctAnswer: 0
+  }
+];
+
+export default questions;
