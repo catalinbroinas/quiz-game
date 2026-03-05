035da6c6811ff1b7ce4bd24fe133fe72c6165479
catalinbroinas <catalinbroinas@yahoo.com>
2026-03-05 12:54:02 +0200

    feat(QuizCategory): add controlled component

--- src/react/components/features/Game/GameSettings/QuizCategory.jsx
+++ src/react/components/features/Game/GameSettings/QuizCategory.jsx
@@ -1,10 +1,18 @@
 
-function QuizCategory() {
+function QuizCategory({ category, onCategoryChange }) {
   return (
     <div className="form-select-group">
-      <label htmlFor="quiz-category" className="form-label-text">Category:</label>
+      <label
+        htmlFor="quiz-category"
+        className="form-label-text"
+      >Category:</label>
 
-      <select id="quiz-category" className="form-select">
+      <select
+        id="quiz-category"
+        className="form-select"
+        value={category}
+        onChange={(e) => onCategoryChange(e.target.value)}
+      >
         <option value="technology">Technology</option>
         <option value="geography">Geography</option>
         <option value="sport">Sport</option>
