5f4d914ef5531037bf96768c82c13b9d623bd3af
catalinbroinas <catalinbroinas@yahoo.com>
2026-03-06 15:09:17 +0200

    feat(GameSettings): submit and apply quiz settings

--- src/react/components/features/Game/Game.jsx
+++ src/react/components/features/Game/Game.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import GameSettings from "./GameSettings/GameSettings";
 import Question from "./Question";
 
 function Game() {
+  const [settings, setSettings] = useState({
+    numQuestions: 0,
+    difficulty: undefined,
+    category: undefined
+  });
+  const [gameStatus, setGameStatus] = useState('idle');
+
+  const handleApplySettings = (newSettings) => {
+    setSettings(newSettings);
+    setGameStatus('playing');
+  };
+
   return (
     <div className="game">
       <h1 className="game__title">Quiz Game</h1>
 
-      <GameSettings />
-      <Question />
+      {gameStatus === 'idle' && (
+        <GameSettings onApply={handleApplySettings} />
+      )}
     </div>
   );
 }
--- src/react/components/features/Game/GameSettings/GameSettings.jsx
+++ src/react/components/features/Game/GameSettings/GameSettings.jsx
@@ -1,16 +1,52 @@
+import { useState } from "react";
 import NumberOfQuestions from "./NumberOfQuestions";
 import DifficultyLevel from "./DifficultyLevel";
 import QuizCategory from "./QuizCategory";
 
-function GameSettings() {
+function GameSettings({ onApply }) {
+  const [settings, setSettings] = useState({
+    numQuestions: 0,
+    difficulty: '',
+    category: ''
+  });
+
+  const normalizeSettings = (settings) => ({
+    ...settings,
+    numQuestions: Number(settings.numQuestions)
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onApply(normalizeSettings(settings));
+  };
+
   return (
     <div className="game__settings">
-      <form className="form-container">
-        <NumberOfQuestions />
-        <DifficultyLevel />
-        <QuizCategory />
+      <form className="form-container" onSubmit={handleSubmit}>
+        <NumberOfQuestions
+          numQuestions={settings.numQuestions}
+          onNumQuestionsChange={(value) =>
+            setSettings(prev => ({ ...prev, numQuestions: value}))
+          }
+        />
 
-        <button className="btn-primary">Start Quiz</button>
+        <DifficultyLevel
+          difficulty={settings.difficulty}
+          onDifficultyChange={(value) =>
+            setSettings(prev => ({ ...prev, difficulty: value}))
+          }
+        />
+
+        <QuizCategory
+          category={settings.category}
+          onCategoryChange={(value) =>
+            setSettings(prev => ({ ...prev, category: value}))
+          }
+        />
+
+        <button type="submit" className="btn-primary">
+          Start Quiz
+        </button>
       </form>
     </div>
   );
