2e7c703a3b47827a5bbf94c9c7e4ad1d0c89a227
catalinbroinas <catalinbroinas@yahoo.com>
2026-03-12 12:59:50 +0200

    feat(utils): add getDifficultiesByCategory helper

--- src/react/utils/questions.js
+++ src/react/utils/questions.js
@@ -15,3 +15,19 @@ export function getFilteredQuestions(questions, filters) {
     question.difficulty === difficulty
   );
 };
+
+/**
+ * Filters difficulty options by selected category.
+ * @param {Array} questions - Array of question objects.
+ * @param {Object} category - Selected category.
+ * @returns {Array} - An array with available difficulty options by selected category.
+ */
+export function getDifficultiesByCategory(questions, category) {
+  return [
+    ...new Set(
+      questions
+        .filter(question => question.category === category)
+        .map(question => question.difficulty)
+    )
+  ];
+}
